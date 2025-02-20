748fe7e0244239477c864f3089b7b5c3d843e9fe
MiniMinja <minyoungheo.heo@gmail.com>
2025-02-20 19:14:01 +0900

    Finished making simple test suite and testing a single function

---
+++ headers/testingSuite.hpp
@@ -0,0 +1,71 @@
+#include <string>
+#include <iostream>
+
+namespace testing{
+/*simply a wrapper for writing out "passed" or "failed"
+so we can copy and paste this function*/
+inline void printCase(std::string nameOfCase, bool passed){
+    if(passed){
+        std::cout << "Passed: ";
+    }
+    else{
+        std::cout << "Failed: ";
+    }
+    std::cout << "nameOfCase"  << std::endl;
+}
+
+/*we can't verify things are working with sockets
+we just want to make sure that it looks right when
+printed
+so we ask the user (the tester) to manually type out yes or no
+if things are working or not*/
+inline bool testViaUserInput(std::string msg){
+    std::string ans = "";
+
+    std::cout << "\t!!!!HEY USER!!!!\n" << msg << " (yes or no) ";
+    std::cin >> ans;
+    while(ans != "yes" && ans != "no"){
+        std::cout << "\t>>Must be yes or no<<" << std::endl;
+        std::cout << msg << " (yes or no) ";
+        std::cin >> ans;
+    }
+    return ans == "yes";
+}
+
+/*A wrapper for our original method. There was so much repetition
+that I wanted to shrink like 4 lines of repeated code to "test"
+
+I think this is as general as I can make it*/
+class TestSuite{
+    private:
+        int passed, total;
+
+    public:
+        inline TestSuite(std::string name, std::string filename){
+            std::cout << "\n=====================================================" << std::endl;
+            std::cout << "Testing " << name <<  " in " << filename << std::endl;
+            std::cout << "=====================================================" << std::endl;
+            passed = 0;
+            total = 0;
+        }
+        inline ~TestSuite(){
+            std::cout << "=====================================================" << std::endl;
+        }
+        TestSuite(const TestSuite&) = delete;
+        TestSuite& operator=(const TestSuite&) = delete;
+
+
+        inline void test(std::string name, bool val){
+            printCase(name, val);
+            if(val){
+                passed++;
+            }
+            total++;
+        }
+
+        inline void printFinalOutput(){
+            std::cout << ">>>Passed: " << passed << "/" << total << std::endl;
+        }
+};
+
+}
---
+++ testing/socketTester.py
@@ -0,0 +1,35 @@
+import errorPythonPort as eport
+
+import socket
+
+OPENED = 1
+CLOSED = 2
+
+'''
+    Checks to see the socket port is open
+
+    Note:
+        This program verifies this by CONNECTING
+        to the port. It's not like netstat that 
+        scans which ports are open
+'''
+def checkSocket(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = s.connect_ex(('127.0.0.1',port))   # this is connect() but without 
+                                                # without raising an exception
+    s.close()
+    if result == 0:
+        return OPENED
+    else:
+        return CLOSED
+
+# Test for socket
+def runTest():
+    print('PYTHON: Started Tester!!!')
+    tester = eport.generateTester({
+        "verifyOpen": lambda args: checkSocket(args[0]) == OPENED, 
+        "verifyClosed": lambda args: checkSocket(args[0]) == CLOSED
+    })
+
+    eport.run(tester)
+
